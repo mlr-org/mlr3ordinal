001a9ef4a6002ffd68175450a1d85c60c8f03ecc
Michel Lang <michellang@gmail.com>
2019-09-04 21:52:14 +0200

    param_vals is deprecated

--- R/LearnerOrdinal.R
+++ R/LearnerOrdinal.R
@@ -23,8 +23,8 @@ NULL
 #' @export
 LearnerOrdinal = R6Class("LearnerOrdinal", inherit = Learner,
   public = list(
-    initialize = function(id, param_set = ParamSet$new(), param_vals = list(), predict_types = "response", feature_types = character(), properties = character(), data_formats = "data.table", packages = character()) {
-      super$initialize(id = id, task_type = "ordinal", param_set = param_set, param_vals = param_vals,
+    initialize = function(id, param_set = ParamSet$new(), predict_types = "response", feature_types = character(), properties = character(), data_formats = "data.table", packages = character()) {
+      super$initialize(id = id, task_type = "ordinal", param_set = param_set,
         predict_types = predict_types, feature_types = feature_types, properties = properties,
         data_formats = data_formats, packages = packages)
     },
