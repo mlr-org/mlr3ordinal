daf9783d7c06976f4a6d4e7fa8496b4e1f3a64f9
Coorsaa <stefan.coors@gmx.net>
2019-10-04 03:56:06 -0600

    added test for probs

--- tests/testthat/test_mlr_learners_ordinal_clm.R
+++ tests/testthat/test_mlr_learners_ordinal_clm.R
@@ -5,3 +5,13 @@ test_that("autotest", {
   result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
+
+test_that("Predict with prob", {
+  task = tsk("winerating")
+  learner = lrn("ordinal.clm", predict_type = "prob")
+  expect_learner(learner, task)
+
+  p = learner$train(task)$predict(task)
+  expect_matrix(p$prob, nrows = 72L, ncols = 5L)
+  expect_names(colnames(p$prob), permutation.of = levels(task$truth()))
+})
