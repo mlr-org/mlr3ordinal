0c006a9944079a85a96d84ffa41b2aa61be3365c
Coorsaa <stefan.coors@gmx.net>
2019-10-03 21:03:00 -0600

    additional tests

--- tests/testthat/test_TaskOrdinal.R
+++ tests/testthat/test_TaskOrdinal.R
@@ -1,9 +1,10 @@
 context("TaskOrdinal")
 
 test_that("Task duplicates rows", {
-  task = mlr_tasks$get("winerating")
+  task = tsk("winerating")
   expect_task(task)
   expect_task_supervised(task)
+  expect_number(task$rank_n())
 
   expect_is(task$truth(), "ordered")
 
@@ -11,4 +12,10 @@ test_that("Task duplicates rows", {
   expect_formula(f)
   expect_set_equal(extract_vars(f)$lhs, task$target_names)
   expect_set_equal(extract_vars(f)$rhs, ".")
+})
+
+test_that("Task errors", {
+  expect_error(TaskOrdinal$new(id = "iris", backend = iris, target = "Species"), "must be an ordered factor")
+  iris$Species = ordered(rep(1, 150))
+  expect_error(TaskOrdinal$new(id = "iris", backend = iris, target = "Species"), "must have at least two levels")
 })
