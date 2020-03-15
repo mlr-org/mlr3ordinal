65865feadd63d68fbd0598aac291689ab0b24183
pat-s <patrick.schratz@gmail.com>
2020-03-15 11:38:44 +0100

    fix tests

--- tests/testthat/test_register.R
+++ tests/testthat/test_register.R
@@ -4,5 +4,5 @@ test_that("re-populate dictionaries", {
   rm("ordinal.clm", envir = mlr_learners$items)
   expect_disjunct("ordinal.clm", mlr_learners$keys())
   register_mlr3()
-  expect_subset("ordinal.clm", mlr_learners$keys())
+  checkmate::expect_subset("ordinal.clm", mlr_learners$keys())
 })
