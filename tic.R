9035f5874fdf34dca9711197087d062942c6e2cf
pat-s <patrick.schratz@gmail.com>
2020-03-15 16:02:47 +0100

    remove travis

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 do_package_checks(error_on = "warning")
 
-if (ci_has_env("BUILD_PKGDOWN")) {
+if (ci_on_ghactons()) {
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
