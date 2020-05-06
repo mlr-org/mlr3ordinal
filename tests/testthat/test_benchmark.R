1bad837a0ad653274bd36d126c9c5df4b0e1fa56
Stefan Coors <stefan.coors@gmx.net>
2020-05-06 10:40:02 +0200

    updates from mlr3pipelines (#8)
    * updates from mlr3pipelines
    
    * changed classif pipeline
    
    * fixed pipelines
    
    * updates
    
    * fixed registry of ordinal graph in mlr_graphs
    
    * depends on pipelines branch

--- tests/testthat/test_benchmark.R
+++ tests/testthat/test_benchmark.R
@@ -1,8 +1,8 @@
 context("benchmark")
 
 test_that("benchmark works", {
-  clrn = GraphLearner$new(PipelineOrdinal(lrn("classif.rpart")), task_type = "ordinal")
-  rlrn = GraphLearner$new(PipelineOrdinal(lrn("regr.rpart")), task_type = "ordinal")
+  clrn = GraphLearner$new(ppl("ordinal", lrn("classif.rpart")), task_type = "ordinal")
+  rlrn = GraphLearner$new(ppl("ordinal", lrn("regr.rpart")), task_type = "ordinal")
   learners = list(lrn("ordinal.clm"), clrn, rlrn)
   task = tsk("winerating")
   bmr = benchmark(benchmark_grid(task, learners, rsmp("cv", folds = 3)))
--- tests/testthat/test_ordinalPipeline.R
+++ tests/testthat/test_ordinalPipeline.R
@@ -2,8 +2,8 @@ context("OrdinalPipeline")
 
 test_that("regression learner", {
   task = tsk("winerating")
-  lrn = lrn("regr.rpart")
-  graph = PipelineOrdinal(lrn)
+  learner = lrn("regr.rpart")
+  graph = ppl("ordinal", learner)
 
   glrn = GraphLearner$new(graph, task_type = "ordinal")
   expect_true(run_experiment(task, glrn)$ok)
@@ -18,8 +18,9 @@ test_that("regression learner", {
 
 test_that("classification learner", {
   task = tsk("winerating")
-  lrn = lrn("classif.rpart")
-  graph = PipelineOrdinal(lrn)
+  learner = lrn("classif.rpart")
+  graph = ppl("ordinal", learner)
+  graph$keep_results = TRUE
 
   glrn = GraphLearner$new(graph, task_type = "ordinal")
   expect_true(run_experiment(task, glrn)$ok)
@@ -29,5 +30,10 @@ test_that("classification learner", {
 
   expect_prediction_ordinal({
     graphpred = glrn$predict(task)
+  })
+
+  # test on subset with less levels
+  expect_prediction_ordinal({
+    graphpred = glrn$predict(task$clone()$filter(rows = c(7:9, 11)))
   })
 })
